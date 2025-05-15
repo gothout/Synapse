3060b1f50993035c5069e53a22498a4f68c8bf5b
Gothout <lucasdanielchavesreal@gmail.com>
2025-05-15 15:37:46 -0300

    feat: cria usuario

--- internal/app/admin/user/dto/user_request.go
+++ internal/app/admin/user/dto/user_request.go
@@ -8,7 +8,6 @@ type AdminUserCreateDTO struct {
 	Email        string `json:"email" binding:"required,email"`
 	Senha        string `json:"senha" binding:"required,min=6"`
 	Numero       string `json:"numero" binding:"required"`
-	Token        string `json:"token" binding:"required"`
 	RuleID       int64  `json:"rule_id" binding:"required"`
 	EnterpriseID int64  `json:"enterprise_id" binding:"required"`
 }
@@ -20,7 +19,6 @@ func (dto AdminUserCreateDTO) ToModel() model.User {
 		Email:        dto.Email,
 		Senha:        dto.Senha,
 		Numero:       dto.Numero,
-		Token:        dto.Token,
 		RuleID:       dto.RuleID,
 		EnterpriseID: dto.EnterpriseID,
 	}
--- internal/app/admin/user/model/user.go
+++ internal/app/admin/user/model/user.go
@@ -1,12 +1,16 @@
 package user
 
+import "time"
+
 type User struct {
-	ID           int64  `json:"id"`
-	Nome         string `json:"nome"`
-	Email        string `json:"email"`
-	Senha        string `json:"senha"`
-	Numero       string `json:"numero"`
-	Token        string `json:"token"`
-	RuleID       int64  `json:"rule_id"`
-	EnterpriseID int64  `json:"enterprise_id"`
+	ID           int64     `json:"id"`
+	Nome         string    `json:"nome"`
+	Email        string    `json:"email"`
+	Senha        string    `json:"senha"`
+	Numero       string    `json:"numero"`
+	Token        string    `json:"token"`
+	RuleID       int64     `json:"rule_id"`
+	EnterpriseID int64     `json:"enterprise_id"`
+	CreatedAt    time.Time `db:"created_at"`
+	UpdatedAt    time.Time `db:"updated_at"`
 }
