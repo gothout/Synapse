db6f83327ff33b7a25d0624593655869c38f27f9
Gothout <lucasdanielchavesreal@gmail.com>
2025-05-23 10:56:35 -0300

    bugfix: corrige middleware para verificar integracao é pertencente a X empresa quando chamado token de integração.

--- internal/app/admin/middleware/service/middleware_service_interface.go
+++ internal/app/admin/middleware/service/middleware_service_interface.go
@@ -9,4 +9,5 @@ type MiddlewareService interface {
 	ValidateToken(ctx context.Context, token string) (int64, error)
 	HasPermission(ctx context.Context, userID int64, module, action string) (bool, error)
 	ValidateTokenIntegration(ctx context.Context, token string) (*model.IntegrationWithPermissions, error)
+	CheckEnterpriseToken(ctx context.Context, token string, integrationID string) (*model.IntegrationWithPermissions, error)
 }
